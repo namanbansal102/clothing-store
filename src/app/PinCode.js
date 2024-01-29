3012fd7be420ad71154fa4e2c1ae021337dfc8e2
Naman <namanbansal102@gmail.com>
2024-01-29 17:57:47 +0530

    Added PinCode Section in Product Page

--- src/app/PinCode.js
+++ src/app/PinCode.js
@@ -1,12 +1,43 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const PinCode = () => {
+  const [pin, setPin] = useState(null);
+  const [avalibiltiy, setavalibiltiy] = useState(false)
+  const handleClick=()=>{
+    
+    console.log("Handle Click is Running");
+    let data=fetch('http://localhost:3000/api/fetchpin', {
+      method: 'POST',
+      headers: {  
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({pin}),
+    }).then((a)=>a.json()).then((json)=>{
+      if (json.status=='available') {
+        setavalibiltiy(true);
+      }
+      else{
+        setavalibiltiy(false);
+      }
+      console.log(json);
+    })
+
+  }
+  const handleChange=(e)=>{
+    setPin(e.target.value)
+  }
   return (
+<>
     <div className='my-6 flex gap-3'>
-    <input placeholder='Enter pin Code here' className='border-2 border-black'></input>
-    <button className='bg-black text-white px-2 py-2 hover:bg-white hover:text-black'>Check</button>
+    <input onChange={handleChange}  placeholder='Enter pin Code here' className='border-2 border-black'></input>
+    <button className='bg-black text-white px-2 py-2 hover:bg-white hover:text-black' onClick={handleClick}>Check</button>
     </div>
+    {avalibiltiy && <h1 className='font-bold'>Delivery Within 3-7 Days</h1>}
+    {pin && !avalibiltiy && <h1 className=' font-bold'>Sorry Not Reached At This Point</h1>}
+</>
   )
+  
 }
 
 export default PinCode
--- src/app/api/fetchpin/route.js
+++ src/app/api/fetchpin/route.js
@@ -1,5 +1,16 @@
 export async function POST(request,response){
-    log
-
-    return Response.json({125102:"Ellenabad",140401:"Rajpura"})
+    let pin=await request.json()
+    pin=pin.pin
+    const myjson={
+        125102:"Ellenabad",140401:"Rajpura",
+        125100:"Sirsa"
+    }
+    if (myjson[pin]!=undefined) {
+        return Response.json({"status":"available"})
+    }
+    else{
+        
+        return Response.json({"status":"unavailable"})
+    }
+    
 }
