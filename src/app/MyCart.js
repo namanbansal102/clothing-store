e6096ba36beeb13404dbd709fa4086613ee85a65
Naman <namanbansal102@gmail.com>
2024-02-01 14:39:00 +0530

    fixed cart page

--- src/app/MyCart.js
+++ src/app/MyCart.js
@@ -1,9 +1,18 @@
 import { faDeleteLeft, faRemove } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const MyCart = (props) => {
   let outlet=props['outlet']
+  const handleDelete=()=>{
+    console.log("Handle Delete is Running");
+    let a=".westside."+(outlet['_id']).substring(0,10)
+    console.log(a);
+    localStorage.removeItem(a)
+    window.location.reload();
+  }
+ 
+  const [val, setval] = useState(1);
   // {_id: '65b7f1ab995e10c4f1419024', title: 'Nuon Brown Abstract Printed Relaxed Fit Shirt', img: 'https://cdn.shopify.com/s/files/1/0266/6276/4597/files/300962224BROWN_1.jpg?v=1704953937', category: 'codes-wear', size: 'codes-wear', …}
   return (
     <div className="mycart shadow-lg my-4 w-fit flex gap-7 items-center px-4">
@@ -18,12 +27,19 @@ const MyCart = (props) => {
             </div>
             <div className="mycart-right flex gap-5 text-xl">
                 <div className="quantity flex gap-5">
-                    <button className='rounded-full border-2 border-gray-100 px-2 '>-</button>
-                    1
-                    <button className='rounded-full border-2 border-gray-100 px-2 '>+</button>
+                    
+                    <button className='rounded-full border-2 border-gray-100 px-2 ' onClick={()=>{
+                      if (val!=0) {
+                        setval(val-1)
+                      }
+                      }}>-</button>
+                    <h1>{val}</h1>
+                    <button className='rounded-full border-2 border-gray-100 px-2 ' onClick={()=>{
+                      if (val!=outlet['quantity']) {
+                      setval(val+1)}}}>+</button>
                 </div>
                 <div className="subtotal">RS.699</div>
-                <div className="Delete"><FontAwesomeIcon icon={faDeleteLeft}></FontAwesomeIcon></div>
+                <div  className="Delete cursor-pointer" onClick={handleDelete}><FontAwesomeIcon icon={faDeleteLeft}></FontAwesomeIcon></div>
             </div>
         </div>
   )
--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -3,19 +3,21 @@ import React, { useEffect, useState } from 'react'
 import MyCart from '../MyCart'
 
 const page =() => {
-  
-  const total=0;
+  const [tempArray, settempArray] = useState([])
+  const [total, settotal] = useState(0)
   const [discount, setdiscount] = useState(0)
-    const [json, setjson] = useState([]);
+  const [json, setjson] = useState([]);
   const [localStorageItems, setlocalStorageItems] = useState([]);
   useEffect(() => {
     const localStorageItemsArray = [];
+    
     for (const localStorageItem of Object.keys(localStorage)) {
       const val = localStorageItem.split(".");
       if (val[1] === 'westside') {
         localStorageItemsArray.push({ id: localStorage.getItem(localStorageItem) });
       }
     }
+    settempArray(localStorageItemsArray)
     console.log("localStorageItemsArray:::::::",localStorageItemsArray);
 
     async function fetchData() {
@@ -37,59 +39,62 @@ const page =() => {
         // Handle errors gracefully, e.g., display an error message
       }
     }
-    for (let i= 0; i < json.length; i++) {
-      const getamount = (json[i])['price'];
-      console.log("Total Amount is:::::::::::::::::::: ",getamount);
-      total+=getamount
-    }
+    
+    
     fetchData();
     
   },[]);
   
   console.log("Json is::::::::::::::::::::::::",json);
-  for (let i= 0; i < json.length; i++) {
-    const getamount = (json[i])['price'];
-    console.log("Total Amount is:::::::::::::::::::: ",getamount);
-    total+=getamount
-  }
+  
   return (
-    <div className='wholeDiv  w-fit mx-4 my-8 flex gap-60'>
-        <div className="whole-cart-big w-fit px-4  rounded-2xl shadow-md">
-        <div className="cart-head flex items-center flex-col ">
-            <div className="cart-head-right text-3xl my-3">Shopping Bag (03)</div>
-            <div className="card-head-left">
-                <ul className='flex gap-5 items-center '>
-                    <li><button className='bg-black w-8 h-8 text-white my-2 mx-2'>1</button>Bag </li>
-                    <div className='border-t-2 border-black w-5 h-0'></div>
-                    <li><button className='bg-black w-8 h-8 text-white my-2 mx-2'>2</button>Details</li>
-                    <div className='border-t-2 border-black w-5 h-0'></div>
-                    <li><button className='bg-black w-8 h-8 text-white my-2 mx-2'>3</button>Payment</li>
-
-                </ul>
-            </div>
-            
-        </div>
-        <div className="myitems flex flex-col gap-4">
+    <>
+    {tempArray.length==0 && <div className='h-full w-full flex justify-around my-10 items-center flex-col gap-9'><img src="https://bakestudio.in/assets/images/cart/empty-cart.gif" alt="" /><h1 className='text-5xl font-bold' >YOUR CART IS EMPTY</h1></div>}
+    {tempArray.length!=0 && <div className='wholeDiv  w-fit mx-4 my-8 flex gap-60'>
+      <div className="whole-cart-big w-fit px-4  rounded-2xl shadow-md">
+      <div className="cart-head flex items-center flex-col ">
+      <div className="cart-head-right text-3xl my-3">Shopping Bag (03)</div>
+      <div className="card-head-left">
+      <ul className='flex gap-5 items-center '>
+      <li><button className='bg-black w-8 h-8 text-white my-2 mx-2'>1</button>Bag </li>
+      <div className='border-t-2 border-black w-5 h-0'></div>
+      <li><button className='bg-black w-8 h-8 text-white my-2 mx-2'>2</button>Details</li>
+      <div className='border-t-2 border-black w-5 h-0'></div>
+      <li><button className='bg-black w-8 h-8 text-white my-2 mx-2'>3</button>Payment</li>
+      
+      </ul>
+      </div>
+      
+      </div>
+      <div className="myitems flex flex-col gap-4">
         {
-            (json).map((element)=>{
-              return <MyCart outlet={element}></MyCart>
-            })
+          (json).map((element)=>{
+            return <MyCart outlet={element}></MyCart>
+          })
         } 
         </div>
         </div>
         <div className="cart-right  text-xl my-32 mx-24  h-fit">
-            <div className="get-amount leading-10">
-
-    <h1 className='font-semibold text-3xl'>Total Amount</h1>
-    <h1>Total Item&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4</h1>
-    <h1>Total Amount&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{total}</h1>
+        <div className="get-amount leading-10">
+        
+        <h1 className='font-semibold text-3xl'>Total Amount</h1>
+        <h1>Total Item&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4</h1>
+        <h1 onLoad={()=>{
+          for (let i = 0; i  < json.length; i++) {
+            let element = (json[i])['price'] ;
+            settotal(total+element)
+          }
+          
+        }}>Total Amount&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{total}</h1>
     <h1>Discount&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0</h1> 
     <h1>Net Amount&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{total}</h1>
-            </div>
-            <button className='bg-black text-white h-12 w-full' >Order Now</button>
-        </div>
     </div>
-  )
-}
-
-export default page
+    <button className='bg-black text-white h-12 w-full' >Order Now</button>
+    </div>
+    </div>
+  }
+  </>
+    )
+  }
+  
+  export default page
