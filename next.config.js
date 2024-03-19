3db6d8e42ff9400c9066478391a60b86940d878b
Sahil <Sahil2003@gmail.com>
2024-03-19 14:05:38 +0530

    bug fixing done

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     env: {
-        NEXT_PUBLIC_MONGO_URI:"mongodb+srv://namanbansal102:y72sh8OeKaR9CyHZ@westside.bmsefdh.mongodb.net/?retryWrites=true&w=majority&appName=westside",
-        NEXT_PUBLIC_HOST:"http://localhost:3000/",
-        CRYPTO_SECRET:"naman#$%@@#!221323%6namanbansal@#EF",
-        JWT_SECRET:"namansecretjwtsecret",
+        NEXT_PUBLIC_MONGO_URI: "mongodb+srv://namanbansal102:y72sh8OeKaR9CyHZ@westside.bmsefdh.mongodb.net/?retryWrites=true&w=majority&appName=westside",
+        NEXT_PUBLIC_HOST: "https://clothing-store-naman.vercel.app/",
+        CRYPTO_SECRET: "naman#$%@@#!221323%6namanbansal@#EF",
+        JWT_SECRET: "namansecretjwtsecret",
         KEY_ID: "rzp_test_FDXSZ76LEo92zE",
         KEY_SECRET: "qWF0L8TlfHw10g9yQoq9s7yB",
-        GOOGLE_CLIENT_ID:"509478278825-5evqs019g7s3j42tm5ddfccs26kr6rdn.apps.googleusercontent.com",
-        NEXT_PUBLIC_RECAPTCHA_KEY:"6Lc--IEpAAAAAEr5QdnxEzdz4Ssr8IEh4l-K806g",
-        NEXT_PUBLIC_CRISP_ID:"a51dc7c3-9789-4335-8fad-43da356a5d4b",
-        REPLICATE_API_TOKEN:"r8_HlMzJ3Nb4iSxw7VC1wpcsQ4ESn33ylW1vf1CR",
-        NEXT_PUBLIC_PASS_EMAIL:'wzxv qfku errq lzwm'
+        GOOGLE_CLIENT_ID: "509478278825-5evqs019g7s3j42tm5ddfccs26kr6rdn.apps.googleusercontent.com",
+        NEXT_PUBLIC_RECAPTCHA_KEY: "6Lc--IEpAAAAAEr5QdnxEzdz4Ssr8IEh4l-K806g",
+        NEXT_PUBLIC_CRISP_ID: "a51dc7c3-9789-4335-8fad-43da356a5d4b",
+        REPLICATE_API_TOKEN: "r8_HlMzJ3Nb4iSxw7VC1wpcsQ4ESn33ylW1vf1CR",
+        NEXT_PUBLIC_PASS_EMAIL: 'wzxv qfku errq lzwm'
     },
-  }
+}
--- src/app/products/[fashion-name]/page.js
+++ src/app/products/[fashion-name]/page.js
@@ -5,24 +5,24 @@ import Product from '@/app/Productcard'
 import FilterBar from '@/app/FilterBar'
 import DataComponent from './[product-name]/components/DataComponent'
 const page = async (slug) => {
-  const k=(slug['params'])["fashion-name"];
-  
-  let data=await fetch(`https://clothing-store-naman.vercel.app/api/getProducts?query=${k}`)
-  let newPromise =  
-  new Promise(function (resolve, reject) { 
-  setTimeout(function () { 
-      resolve("Hello Geeks. Wrapped  setTimeout() in a promise"); 
-  }, 500); 
-}); 
-let result = await newPromise; 
-  let json=await data.json()
-    return (
-      <>
+  const k = (slug['params'])["fashion-name"];
+
+  let data = await fetch(`${process.env.NEXT_PUBLIC_HOST}api/getProducts?query=${k}`)
+  let newPromise =
+    new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve("Hello Geeks. Wrapped  setTimeout() in a promise");
+      }, 500);
+    });
+  let result = await newPromise;
+  let json = await data.json()
+  return (
+    <>
       <div>
         <FilterBar outlet={json['products']}>
-        </FilterBar> 
-        </div>
-        </>
+        </FilterBar>
+      </div>
+    </>
   )
 }
 export default page
